21fe114b91f00a01e16ab101798bf385d941c47f
Fireentity_293_ <croceclaudio57@gmail.com>
2022-02-22 16:57:25 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='py-launcher',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='lorenzo',
+    author_email='croceclaudio57@gmail.com',
+    description='A simple python launcher'
+)
