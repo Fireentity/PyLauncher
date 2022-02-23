2175de53747fddfe236ff89edc68161599a59c44
Fireentity_293_ <croceclaudio57@gmail.com>
2022-02-23 19:24:24 +0100

    test

--- setup.py
+++ setup.py
@@ -10,5 +10,6 @@ setup(
     license='MIT',
     author='lorenzo',
     author_email='croceclaudio57@gmail.com',
-    description='A simple python launcher'
+    description='A simple python launcher',
+    include_package_data=True
 )
