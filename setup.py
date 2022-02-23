ada81c06942b48df5a8b59c5ae1995f48fa8f100
Fireentity_293_ <croceclaudio57@gmail.com>
2022-02-23 19:42:24 +0100

    test

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ setup(
     name='PyLauncher',
     version='1.0.0',
     packages=find_packages(),
-    data_files=[("/home/lorenzo", ["PyLauncher/configs/config.json"])],
     url='',
     license='MIT',
     author='lorenzo',
